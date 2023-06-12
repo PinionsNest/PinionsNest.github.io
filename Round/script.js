4d21fefd343065f130b0b1a1a3d1c6d08c643620
PinionsNest <135445676+PinionsNest@users.noreply.github.com>
2023-06-12 17:17:56 +0600

    removed move

--- Round/script.js
+++ Round/script.js
@@ -7,23 +7,19 @@ const balls = [];
 let div = document.createElement("div");
 let button = document.getElementById("c")
 document.body.appendChild(div);
-let numberofvballs = 30;
+let numberofballs = prompt("How many balls do u want? [Number Only]");
 
-function move() {
-  centerX = Math.random() * (750 - 550) + 550;
-  centerY = Math.random() * (400 - 200) + 200;
-}
 
 function start() {
     button.remove()
-for (let i = 0; i < numberofvballs; i++) {
-  speed.push((0.001 * numberofvballs) - (i * 0.001));
+for (let i = 0; i < numberofballs; i++) {
+  speed.push((0.001 * numberofballs) - (i * 0.001));
   angle.push(0);
   let ball = document.createElement("p");
   div.appendChild(ball);
   balls.push(ball);
   animate(i);
-  setInterval(move, 1000);
+
 }
 
 function animate(index) {
