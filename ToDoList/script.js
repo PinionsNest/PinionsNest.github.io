2468797564feab5d39a585c3bc95e3b94b70e328
PinionsNest <135445676+PinionsNest@users.noreply.github.com>
2023-07-14 22:22:59 +0600

    Renamed the variables

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,96 +1,89 @@
-const listInput = document.getElementById("listInput");
-const output = document.getElementById("show");
-const p1 = document.getElementById("p")
-const n1 = document.getElementById("n2");
-const m = document.getElementById("audio")
-
+const inputField = document.getElementById("listInput");
+const outputTable = document.getElementById("show");
+const statusParagraph = document.getElementById("p");
+const toggleButton = document.getElementById("n2");
+const audioPlayer = document.getElementById("audio");
 
 function submitList(event) {
   //Used to prevent the page from refreshing every time the form is submitted.
   event.preventDefault();
 
-  const todo = {
-    text: listInput.value,
+  const todoItem = {
+    text: inputField.value,
     notdone: "Ongoing...",
     done: "Done",
   };
 
   // Getting the value from the input box.
-  const x = todo.text;
+  const inputValue = todoItem.text;
 
   // Creating all the additional elements.
-  const y = document.createElement("tr");
-  const z1 = document.createElement("td");
-  const z2 = document.createElement("td");
-  const z3 = document.createElement("td");
-  const z31 = document.createElement("button");
+  const tableRow = document.createElement("tr");
+  const statusCell = document.createElement("td");
+  const todoCell = document.createElement("td");
+  const deleteCell = document.createElement("td");
+  const deleteButton = document.createElement("button");
 
   // Getting the table tag into a variable.
 
-
   // Removing border from the delete column.
-  z3.setAttribute("style", "background: linear-gradient(to right, #46237a00, #8a4fff00);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.0);text-align:start;");
+  deleteCell.setAttribute("style", "background: linear-gradient(to right, #46237a00, #8a4fff00);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.0);text-align:start;");
 
   // Adding new elements into the table tag.
-  y.appendChild(z1);
-  y.appendChild(z2);
-  y.appendChild(z3);
-  z3.appendChild(z31);
-  output.appendChild(y);
+  tableRow.appendChild(statusCell);
+  tableRow.appendChild(todoCell);
+  tableRow.appendChild(deleteCell);
+  deleteCell.appendChild(deleteButton);
+  outputTable.appendChild(tableRow);
 
   // Adding text to the elements.
-  z1.textContent = todo.notdone;
-  z2.textContent = x;
-  z31.textContent = "Delete";
+  statusCell.textContent = todoItem.notdone;
+  todoCell.textContent = inputValue;
+  deleteButton.textContent = "Delete";
 
   // Resetting the input value.
-  listInput.value = "";
+  inputField.value = "";
 
   // Making the delete button delete the whole row.
-  z31.addEventListener("click", function () {
-    y.remove();
+  deleteButton.addEventListener("click", function () {
+    tableRow.remove();
   });
 
   //Function to change color according to status.
-  function colorChange() {
-    if (z1.textContent === "Ongoing...") {
-      z1.setAttribute("style", "background: linear-gradient(to right, #46237a00, #8a4fff00);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);");
-      z2.setAttribute("style", "background: linear-gradient(to right, #46237a00, #8a4fff00);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);");
-      z1.textContent = "Done";
-    } else if (z1.textContent === "Done") {
-      z1.setAttribute("style", "background: linear-gradient(to right, #46237a, #8a4fff);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);");
-      z2.setAttribute("style", "background: linear-gradient(to right, #46237a, #8a4fff);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);");
-      z1.textContent = "Ongoing...";
+  function changeColor() {
+    if (statusCell.textContent === "Ongoing...") {
+      statusCell.setAttribute("style", "background: linear-gradient(to right, #46237a00, #8a4fff00);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);");
+      todoCell.setAttribute("style", "background: linear-gradient(to right, #46237a00, #8a4fff00);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);");
+      statusCell.textContent = "Done";
+    } else if (statusCell.textContent === "Done") {
+      statusCell.setAttribute("style", "background: linear-gradient(to right, #46237a, #8a4fff);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);");
+      todoCell.setAttribute("style", "background: linear-gradient(to right, #46237a, #8a4fff);box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);");
+      statusCell.textContent = "Ongoing...";
     }
   }
 
-  // Recoloring the status colomn to indicate that it is done.
-  z1.addEventListener("click", colorChange);
+  // Recoloring the status column to indicate that it is done.
+  statusCell.addEventListener("click", changeColor);
 
-  // Recoloring the todolist column to indicate that it is done.
-  z2.addEventListener("click", colorChange);
+  // Recoloring the todo list column to indicate that it is done.
+  todoCell.addEventListener("click", changeColor);
 }
 
+console.log("Wtf are you doing in the console?")
 
+function toggleMusic() {
+  if (toggleButton.textContent === "Music: Off") {
+    audioPlayer.setAttribute("autoplay", "");
+    audioPlayer.setAttribute("loop", "");
+    audioPlayer.setAttribute("src", "backgroundmusic.mp3");
+    audioPlayer.setAttribute("type", "audio/mp3");
 
-console.log("Wtf are yo doing in the console?")
+    audioPlayer.volume = 0.25;
 
+    toggleButton.textContent = "Music: On";
+  } else {
+    audioPlayer.volume = 0;
 
-function on() {
-  if (n1.textContent === "Music: Off") {
-    // m.setAttribute("controls", "")
-    m.setAttribute("autoplay", "")
-    m.setAttribute("loop", "")
-    m.setAttribute("src", "backgroundmusic.mp3")
-    m.setAttribute("type", "audio/mp3")
-
-    m.volume = 0.25;
-
-    n1.textContent = "Music: On";
+    toggleButton.textContent = "Music: Off";
   }
-  else{
-    m.volume = 0;
-
-    n1.textContent = "Music: Off"
-  }
-} 
+}
